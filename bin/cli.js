fe573db5d90ac5f5c1ba855a61c00a778d2a6166
James Halliday <mail@substack.net>
2012-01-25 03:44:42 -0800

    control and git ports

--- bin/cli.js
+++ bin/cli.js
@@ -2,6 +2,8 @@
 var argv = require('optimist').argv;
 var figc = require('figc');
 var propagit = require('../');
+var spawn = require('child_process').spawn;
+var path = require('path');
 
 var cmd = argv._[0];
 if (argv._[1]) {
@@ -10,12 +12,12 @@ if (argv._[1]) {
 
 if (cmd === 'drone') {
     var s = argv.hub.toString().split(':');
-    var opts = {
+    var hub = {
         host : s[1] ? s[0] : 'localhost',
         port : parseInt(s[1] || s[0], 10),
     };
     
-    propagit(argv.secret).connect(opts, function (c) {
+    propagit(argv).connect(hub, function (c) {
         c.on('error', function (err) {
             console.error(err && err.stack || err);
         });
@@ -32,8 +34,11 @@ if (cmd === 'drone') {
             console.log('disconnected from the hub');
         });
         
-        c.on('spawn', function (cmd, args, emit) {
-            var ps = spawn(cmd, args);
+        function spawner (cmd, args, emit, opts) {
+            if (!opts) opts = { cwd : c.repodir };
+console.dir([ cmd, args, opts ]);
+            var ps = spawn(cmd, args, opts);
+            
             if (typeof emit === 'function') {
                 ps.stdout.on('data', function (buf) {
                     emit('data', buf.toString());
@@ -41,8 +46,12 @@ if (cmd === 'drone') {
                 ps.stderr.on('data', function (buf) {
                     emit('data', buf.toString());
                 });
-                ps.stdout.on('end', function () { emit('end') });
-                ps.stderr.on('end', function () { emit('end') });
+                var pending = 2;
+                var onend = function () {
+                    if (--pending === 0) emit('end')
+                };
+                ps.stdout.on('end', onend);
+                ps.stderr.on('end', onend);
             }
             else if (typeof emit === 'object') {
                 if (typeof emit.stdout === 'function') {
@@ -51,9 +60,42 @@ if (cmd === 'drone') {
                     });
                 }
             }
+        }
+        c.on('spawn', spawner);
+        
+        c.on('clone', function (repo, emit) {
+            path.exists(path.join(c.repodir, repo), function (ex) {
+                if (ex) {
+                    if (typeof emit === 'function') {
+                        emit('end');
+                    }
+                    else if (typeof emit === 'object') {
+                        if (emit.stdout) emit.stdout('end');
+                        if (emit.stderr) emit.stderr('end');
+                    }
+                }
+                else spawner('git', [
+                    'clone',
+                    'http://' + hub.host + ':' + c.ports.git + '/' + repo,
+                    '--bare'
+                ], emit);
+            });
+        });
+        
+        c.on('pull', function (repo, emit) {
+            spawner('git', [
+                'pull',
+                'http://' + hub.host + ':' + c.ports.git + '/' + repo,
+                'master'
+            ], emit, { cwd : path.join(c.repodir, repo) });
         });
     });
 }
 else if (cmd === 'hub') {
-    propagit(argv.secret).listen(argv.port);
+    var cport = argv.cport || argv.port;
+    var gport = argv.gport || cport + 1;
+    
+    propagit(argv).listen(cport, gport);
+    console.log('control service listening on :' + cport);
+    console.log('git service listening on :' + gport);
 }
--- index.js
+++ index.js
@@ -1,15 +1,21 @@
-var gitEmit = require('git-emit');
-var pushover = require('pushover');
 var dnode = require('dnode');
 var upnode = require('upnode');
 var EventEmitter = require('events').EventEmitter;
+var pushover = require('pushover');
+var fs = require('fs');
+var mkdirp = require('mkdirp');
 
 module.exports = function (secret) {
     return new Propagit(secret);
 };
 
-function Propagit (secret) {
-    this.secret = secret;
+function Propagit (opts) {
+    if (typeof opts === 'string') {
+        opts = { secret : opts };
+    }
+    this.secret = opts.secret;
+    this.repodir = opts.repodir || process.cwd() + '/repos';
+    mkdirp(this.repodir);
 }
 
 Propagit.prototype = new EventEmitter;
@@ -32,8 +38,18 @@ Propagit.prototype.connect = function () {
     });
     
     var inst = upnode(function (remote, conn) {
+        self.ports = remote.ports;
+        
         this.spawn = function (cmd, args, emit) {
             self.emit('spawn', cmd, args, emit);
+        };
+        
+        this.clone = function (repo, emit) {
+            self.emit('clone', repo, emit);
+        };
+        
+        this.pull = function (repo, emit) {
+            self.emit('pull', repo, emit);
         };
     });
     var hub = self.hub = inst.connect.apply(inst, args);
@@ -46,17 +62,50 @@ Propagit.prototype.connect = function () {
     return self;
 };
 
-Propagit.prototype.listen = function (port) {
+Propagit.prototype.listen = function (controlPort, gitPort) {
     var self = this;
+    self.drones = [];
+    
     var server = dnode(function (remote, conn) {
         this.auth = function (secret, cb) {
             if (typeof cb !== 'function') return
             else if (self.secret === secret) {
-                cb(null, {});
+                self.drones.push(remote);
+                conn.on('end', function () {
+                    var ix = self.drones.indexOf(remote);
+                    if (ix >= 0) self.drones.splice(ix, 1);
+                });
+                
+                cb(null, {
+                    ports : {
+                        control : controlPort,
+                        git : gitPort,
+                    },
+                });
+                
+                fs.readdir(self.repodir, function (err, repos) {
+                    if (err) console.error(err)
+                    else repos.forEach(function (repo) {
+                        console.log('clone ' + repo);
+                        remote.clone(
+                            repo,
+                            process.stdout.emit.bind(process.stdout)
+                        );
+                    });
+                });
             }
             else cb('ACCESS DENIED')
         };
     });
     server.use(upnode.ping);
-    server.listen(port);
+    server.listen(controlPort);
+    
+    var repos = self.repos = pushover(self.repodir);
+    repos.on('push', function (repo) {
+        self.emit('push', repo);
+        self.drones.forEach(function (drone) {
+            drone.spawn();
+        });
+    });
+    repos.listen(gitPort);
 };
